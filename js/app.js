479c19bcca75feeb396f0b64f86b08f4600fa3e0
lautarocantero <lautaroncantero@gmail.com>
2021-03-25 00:56:42 -0300

    agregado async await

--- js/app.js
+++ js/app.js
@@ -49,20 +49,29 @@ function mostrarAlerta(mensaje){
 
 }
 
-function buscarImagenes(){
+async function buscarImagenes(){
 
     const termino = document.querySelector('#termino').value;
 
     const key = '20796340-393e3765d24ca4e9b62c58f51';
     const url =`https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
 
-    fetch(url)
-        .then( respuesta => respuesta.json())
-        .then( resultado => {
-            totalPaginas = calcularPaginas(resultado.totalHits);
-            console.log(totalPaginas);
-            mostrarImagenes(resultado.hits)
-        });
+    // fetch(url)               SIN ASYNC AWAIT
+    //     .then( respuesta => respuesta.json())
+    //     .then( resultado => {
+    //         totalPaginas = calcularPaginas(resultado.totalHits);
+    //         console.log(totalPaginas);
+    //         mostrarImagenes(resultado.hits)
+    //     });
+
+    try {
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        totalPaginas = calcularPaginas(resultado.totalHits);
+        mostrarImagenes(resultado.hits)
+    } catch (error) {
+        console.log('error')
+    }
 
 }
 
